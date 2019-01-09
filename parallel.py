9771a10dc85c923484156b08c3be785aaed5a952
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2019-01-09 12:34:17 +0800

    Add files via upload

---
+++ parallel.py
@@ -0,0 +1,19 @@
+import os
+from obspy.core import *
+
+# parallel params
+start_date = '20160901'
+end_date   = '20180901'
+num_thread = 10
+
+ts = UTCDateTime(start_date)
+te = UTCDateTime(end_date)
+dt = (te-ts) /num_thread
+for i in range(num_thread):
+    t0 = str((ts + i*dt).date)
+    t1 = str((ts + (i+1)*dt).date)
+    os.system('python mkctlg.py --time_range={},{} \
+               --out_pha=./output/phase_{}_{}.dat \
+               --out_ctlg=./output/catalog_{}_{}.dat &' \
+               .format(t0,t1,t0,t1,t0,t1))
+
