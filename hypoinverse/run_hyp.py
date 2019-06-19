9a9b2664f7bab69d7458c5515981131dbe590115
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2019-06-19 20:31:14 +0800

    Add files via upload

--- hypoinverse/run_hyp.py
+++ hypoinverse/run_hyp.py
@@ -1,10 +1,31 @@
-import os
+import os, glob
+import numpy as np
 import subprocess
-fhyp='xj.hyp'
+
+# format input
 os.system('python mk_sta.py')
 os.system('python mk_phs.py')
-p = subprocess.Popen(['hyp1.40'], stdin=subprocess.PIPE)
-s = "@{}".format(fhyp)
-p.communicate(s.encode())
-os.system('python sum2csv.py')
+for fname in glob.glob('xj_*.sum'): os.unlink(fname)
 
+for ztri in np.arange(0,40,1):
+    # set control file
+    fhyp0='xj.hyp'
+    fhyp='xj.hyp.tmp'
+    f=open(fhyp0); lines=f.readlines(); f.close()
+    out=open(fhyp,'w')
+    for line in lines:
+        info = line.split()
+        if len(info)==0: continue
+        if info[0] == 'ZTR': line = 'ZTR %s\n'%ztri
+        if info[0] == 'SUM': line = "SUM 'xj_%s.sum'\n"%ztri
+        out.write(line)
+    out.close()
+    os.rename(fhyp, fhyp0)
+
+    # run hypoinverse
+    p = subprocess.Popen(['hyp1.40'], stdin=subprocess.PIPE)
+    s = "@{}".format(fhyp0)
+    p.communicate(s.encode())
+
+# format output
+os.system('python sum2csv.py')
--- hypoinverse/sum2csv.py
+++ hypoinverse/sum2csv.py
@@ -1,9 +1,7 @@
-fname = 'xj.sum'
-fout  = 'xj.csv'
-f=open(fname); lines=f.readlines(); f.close()
-out=open(fout,'w')
+import glob, os
+import numpy as np
 
-for line in lines:
+def write(out, line, mag):
     info = line.split()
     date = info[0]
     hrmn = info[1]
@@ -16,5 +14,40 @@ for line in lines:
     lon_min = float(line.split('E')[1].split()[0])
     lon = lon_deg + lon_min/60
     dep = float(line.split('E')[1].split()[1])
-    out.write('{},{:.4f},{:.4f},{:.1f}\n'.format(dtime, lat, lon, dep))
+    mag = float(mags[i].split(',')[-2])
+    out.write('{},{:.4f},{:.4f},{:.1f},{}\n'.format(dtime, lat, lon, dep+2.7, mag))
 
+# i/o paths
+net = 'zsy3'
+fnames = glob.glob('xj_*.sum')
+fmag  = '../output/%s/catalog_xj_%s.dat'%(net,net)
+fout1 = 'xj_%s.csv'%net
+fout2 = 'xj.sum'
+f=open(fmag);  mags=f.readlines();  f.close()
+out1 = open(fout1,'w')
+out2 = open(fout2,'w')
+
+# read sum files
+sum_files = []
+for fname in fnames:
+    f=open(fname); lines=f.readlines(); f.close()
+    if len(lines)!=len(mags): continue
+    sum_files.append(lines)
+
+for i,mag in enumerate(mags):
+  sum_lines = []
+  for sum_file in sum_files:
+    line = sum_file[i]
+    is_loc = 1
+    if '-' in line.split() or '#' in line.split(): is_loc = 0
+    rms = float(line.split('E')[1].split()[6])
+    sum_lines.append((line, is_loc, rms))
+  sum_lines = np.array(sum_lines, 
+                       dtype=[('line','O'),('is_loc','O'),('rms','O')])
+  sum_lines_loc = sum_lines[sum_lines['is_loc']==1]
+  if len(sum_lines_loc)==0: sum_lines_loc = sum_lines
+  sum_lines = np.sort(sum_lines_loc, order='rms')
+  write(out1, sum_lines[0]['line'], mag)
+  out2.write(sum_lines[0]['line'])
+out1.close()
+out2.close()
