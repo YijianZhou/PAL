d249380d5cf54f34786e1fa080109101889c0f1e
Yijian ZHOU <zhouyj.pku@gmail.com>
2020-01-27 11:35:05 +0800

    Add files via upload

--- hypodd/reloc2csv.py
+++ hypodd/reloc2csv.py
@@ -11,13 +11,14 @@ f=open(freloc); lines=f.readlines(); f.close()
 
 for line in lines:
     codes = line.split()
+    evid = codes[0]
     # get loc info
     lat, lon, dep = codes[1:4]
-    dep = float(dep) - dep_corr
+    dep = round(float(dep) - dep_corr, 2)
     mag = codes[16]
     # get time info
     year, mon, day, hour, mnt, sec = codes[10:16]
     ot = '{}{:0>2}{:0>2}{:0>2}{:0>2}{:0>6}'.format(year, mon, day, hour, mnt, sec)
-    out_csv.write('{},{},{},{},{}\n'.format(ot, lat, lon, dep, mag))
+    out_csv.write('{},{},{},{},{},{}\n'.format(evid, ot, lat, lon, dep, mag))
 
 out_csv.close()
