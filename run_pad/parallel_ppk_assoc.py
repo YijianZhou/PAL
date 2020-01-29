0eae2f18b3da0c4ce0387dcdcdb694b186aec7e0
Yijian ZHOU <zhouyj.pku@gmail.com>
2020-01-30 00:22:01 +0800

    Update parallel_ppk_assoc.py

--- run_pad/parallel_ppk_assoc.py
+++ run_pad/parallel_ppk_assoc.py
@@ -8,16 +8,17 @@ data_dir = '/data3/XLS_SAC/*/*'
 date_rng = '20150901-20190601'
 num_proc = 20
 out_root = './output/xls'
-out_pha = '{}/phase_{}.dat'.format(out_root, date_rng)
-out_ctlg = '{}/catalog_{}.dat'.format(out_root, date_rng)
 out_ppk_dir = '{}/picks'.format(out_root)
 
 # divide by time
-ts, te = [UTCDateTime(date) for date in date_rng.split('-')]
-dt = (te-ts) / num_proc
+start_date, end_date = [UTCDateTime(date) for date in date_rng.split('-')]
+dt = (end_date - start_date) / num_proc
 for i in range(num_proc):
-    t0 = str((ts + i*dt).date)
-    t1 = str((ts + (i+1)*dt).date)
+    t0 = ''.join(str((ts + i*dt).date).split('-'))
+    t1 = ''.join(str((ts + (i+1)*dt).date).split('-'))
+    date_rng = '{}-{}'.format(t0, t1)
+    out_pha = '{}/phase_{}.dat'.format(out_root, date_rng)
+    out_ctlg = '{}/catalog_{}.dat'.format(out_root, date_rng)
     os.system("python {}/run_ppk_assoc.py \
         --date_range={} --data_dir={} \
         --out_pha={} --out_ctlg={} --out_ppk_dir={} &"
