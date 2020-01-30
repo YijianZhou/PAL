65765144847e0350553219235f91991dec2c6531
Yijian ZHOU <zhouyj.pku@gmail.com>
2020-01-30 09:12:18 +0800

    Add files via upload

--- hypodd/mk_pha.py
+++ hypodd/mk_pha.py
@@ -1,5 +1,6 @@
 """ Make phase file for ph2dt
 """
+import os
 from obspy import UTCDateTime
 import config
 
@@ -13,27 +14,27 @@ out=open(fout,'w')
 
 evid = 0
 for line in lines:
-  codes = line.split(',')
-  if len(codes)==5:
-    # write head line
-    evid += 1
-    ot, lat, lon, dep, mag = line.split(',')
-    # format time info
-    ot = UTCDateTime(codes[0])
-    date = '{} {:2} {:2}'.format(ot.year, ot.month, ot.day)
-    time = '{:2} {:2} {:5.2f}'.format(ot.hour, ot.minute, ot.second + ot.microsecond/1e6)
-    # format loc info
-    lat, lon, dep, mag = [float(code) for code in codes[1:]]
-    loc = '{:7.4f} {:9.4f}  {:6.2f} {:4.2f}'.format(lat, lon, dep+dep_corr, mag)
-    out.write('# {} {}  {}  0.00  0.00  0.00  {:>9}\n'.format(date, time, loc, evid))
-  else:
+    codes = line.split(',')
+    if len(codes)==5:
+        # write head line
+        ot, lat, lon, dep, mag = line.split(',')
+        # format time info
+        ot = UTCDateTime(codes[0])
+        date = '{} {:2} {:2}'.format(ot.year, ot.month, ot.day)
+        time = '{:2} {:2} {:5.2f}'.format(ot.hour, ot.minute, ot.second + ot.microsecond/1e6)
+        # format loc info
+        lat, lon, dep, mag = [float(code) for code in codes[1:]]
+        loc = '{:7.4f} {:9.4f}  {:6.2f} {:4.2f}'.format(lat, lon, dep+dep_corr, mag)
+        out.write('# {} {}  {}  0.00  0.00  0.00  {:>9}\n'.format(date, time, loc, evid))
+        evid += 1
+    else:
     # write sta pick lines
-    sta = codes[1]
-    tp, ts = [UTCDateTime(code) for code in codes[2:4]]
-    ttp = tp - ot
-    tts = ts - ot
-    wp, ws = 1., 1.
-    out.write('{:<5}{}{:6.3f}  {:6.3f}   P\n'.format(sta, ' '*6, ttp, wp))
-    out.write('{:<5}{}{:6.3f}  {:6.3f}   S\n'.format(sta, ' '*6, tts, ws))
+        sta = codes[1]
+        tp, ts = [UTCDateTime(code) for code in codes[2:4]]
+        ttp = tp - ot
+        tts = ts - ot
+        wp, ws = 1., 1.
+        out.write('{:<5}{}{:6.3f}  {:6.3f}   P\n'.format(sta, ' '*6, ttp, wp))
+        out.write('{:<5}{}{:6.3f}  {:6.3f}   S\n'.format(sta, ' '*6, tts, ws))
 
 out.close()
