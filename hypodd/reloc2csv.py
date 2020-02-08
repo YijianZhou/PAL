2533ba2842861e96455db7d255336833fc3a4ac6
Yijian ZHOU <zhouyj.pku@gmail.com>
2020-02-08 13:48:51 +0800

    Update reloc2csv.py

--- hypodd/reloc2csv.py
+++ hypodd/reloc2csv.py
@@ -39,6 +39,7 @@ for line in lines:
     mag = codes[16]
     # get time info
     year, mon, day, hour, mnt, sec = codes[10:16]
+    sec = '59.999' if sec=='60.000' else sec
     ot = '{}{:0>2}{:0>2}{:0>2}{:0>2}{:0>6}'.format(year, mon, day, hour, mnt, sec)
     out_ctlg.write('{},{},{},{},{},{}\n'.format(event_name, ot, lat, lon, dep, mag))
     out_pha.write('{},{},{},{},{},{}\n'.format(event_name, ot, lat, lon, dep, mag))
