cd03dc3d8500bd55693f075670aa4544d3b9ace3
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2021-03-17 12:14:30 +0800

    update example workdir

--- example_pal_workdir/parallel_assoc_example.py
+++ example_pal_workdir/parallel_assoc_example.py
@@ -2,13 +2,13 @@ import os, shutil
 from obspy import UTCDateTime
 
 # parallel params
-pal_dir = '/home/zhouyj/software/PAL'
-shutil.copyfile('config_temp.py', os.path.join(pal_dir, 'config.py'))
-time_range = '20180515-20180702'
-num_workers = 10
-out_root = 'output/example'
-pick_dir = 'input/picks'
-sta_file = 'input/example_pal.sta'
+pad_dir = '/home/zhouyj/software/PAL'
+shutil.copyfile('config_rc.py', os.path.join(pad_dir, 'config.py'))
+time_range = '20190704-20190725'
+num_workers = 21
+out_root = 'output/rc'
+pick_dir = 'output/rc/picks'
+sta_file = 'input/rc_scsn_pad.sta'
 
 # divide by time
 start_date, end_date = [UTCDateTime(date) for date in time_range.split('-')]
@@ -17,9 +17,9 @@ for proc_idx in range(num_workers):
     t0 = ''.join(str((start_date + proc_idx*dt).date).split('-'))
     t1 = ''.join(str((start_date + (proc_idx+1)*dt).date).split('-'))
     time_range = '{}-{}'.format(t0, t1)
-    out_pha = '{}/phase_{}.dat'.format(out_root, time_range)
-    out_ctlg = '{}/catalog_{}.dat'.format(out_root, time_range)
+    out_pha = '{}/phase_{}.dat'.format(out_root, t0)
+    out_ctlg = '{}/catalog_{}.dat'.format(out_root, t0)
     os.system("python {}/run_assoc.py \
-        --time_range={} --ppk_dir={} --sta_file={} \
-        --out_ctlg={} --out_pha={} &" \
-        .format(pal_dir, time_range, pick_dir, sta_file, out_ctlg, out_pha))
+        --time_range={} --pick_dir={} --sta_file={} \
+        --out_ctlg={} --out_pha={} & " \
+        .format(pad_dir, time_range, pick_dir, sta_file, out_ctlg, out_pha))
--- example_pal_workdir/parallel_pick_assoc_example.py
+++ example_pal_workdir/parallel_pick_assoc_example.py
@@ -3,13 +3,13 @@ from obspy import UTCDateTime
 
 # parallel params
 pal_dir = '/home/zhouyj/software/PAL'
-shutil.copyfile('config_temp.py', os.path.join(pal_dir, 'config.py'))
-data_dir = '/data' # root dir of data
-time_range = '20190704-20190717'
-sta_file = 'input/example_pal.sta'
-num_workers = 13
-out_root = 'output'
-out_pick_dir = 'output/picks'
+shutil.copyfile('config_rc.py', os.path.join(pal_dir, 'config.py'))
+data_dir = '/data2/Ridgecrest'
+time_range = '20190704-20190725'
+sta_file = 'input/rc_scsn_pal.sta'
+num_workers = 21
+out_root = 'output/rc_kurt'
+out_pick_dir = 'output/rc_kurt/picks'
 
 # divide by time
 start_date, end_date = [UTCDateTime(date) for date in time_range.split('-')]
@@ -18,10 +18,10 @@ for proc_idx in range(num_workers):
     t0 = ''.join(str((start_date + proc_idx*dt).date).split('-'))
     t1 = ''.join(str((start_date + (proc_idx+1)*dt).date).split('-'))
     time_range = '{}-{}'.format(t0, t1)
-    out_pha = '{}/phase_{}.dat'.format(out_root, time_range)
-    out_ctlg = '{}/catalog_{}.dat'.format(out_root, time_range)
+    out_pha = '{}/phase_{}.dat'.format(out_root, t0)
+    out_ctlg = '{}/catalog_{}.dat'.format(out_root, t0)
     os.system("python {}/run_pick_assoc.py \
         --time_range={} --data_dir={} --sta_file={} \
         --out_pick_dir={} --out_ctlg={} --out_pha={} & " \
         .format(pal_dir, time_range, data_dir, sta_file, out_pick_dir, out_ctlg, out_pha))
-
+    
