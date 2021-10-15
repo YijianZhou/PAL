d246ea954a99a15caea989b139d4bee70e38e8dd
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2021-10-14 20:57:54 -0700

    update example

--- example_pal_workdir/parallel_pick_assoc_eg.py
+++ example_pal_workdir/parallel_pick_assoc_eg.py
@@ -3,12 +3,13 @@ from obspy import UTCDateTime
 
 # parallel params
 pal_dir = '/home/zhouyj/software/PAL'
-shutil.copyfile('config_example.py', os.path.join(pal_dir, 'config.py'))
+shutil.copyfile('config_eg.py', os.path.join(pal_dir, 'config.py'))
+data_dir = '/data/Example_data'
 time_range = '20190704-20190707'
-num_workers = 3
-out_root = 'output/example'
-pick_dir = 'output/example/picks'
 sta_file = 'input/example_pal.sta'
+num_workers = 3
+out_root = 'output/eg'
+out_pick_dir = 'output/eg/picks'
 
 # divide by time
 start_date, end_date = [UTCDateTime(date) for date in time_range.split('-')]
@@ -19,7 +20,8 @@ for proc_idx in range(num_workers):
     time_range = '{}-{}'.format(t0, t1)
     out_pha = '{}/phase_{}.dat'.format(out_root, time_range)
     out_ctlg = '{}/catalog_{}.dat'.format(out_root, time_range)
-    os.system("python {}/run_assoc.py \
-        --time_range={} --pick_dir={} --sta_file={} \
-        --out_ctlg={} --out_pha={} & " \
-        .format(pal_dir, time_range, pick_dir, sta_file, out_ctlg, out_pha))
+    os.system("python {}/run_pick_assoc.py \
+        --time_range={} --data_dir={} --sta_file={} \
+        --out_pick_dir={} --out_ctlg={} --out_pha={} & " \
+        .format(pal_dir, time_range, data_dir, sta_file, out_pick_dir, out_ctlg, out_pha))
+    
