64ef795f33cf90322ff324466035ea5da86f233b
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-07-01 17:44:05 -0700

    Update mk_sta.py

--- hypodd/mk_sta.py
+++ hypodd/mk_sta.py
@@ -6,12 +6,15 @@ import config
 # i/o paths
 cfg = config.Config()
 fout=open('input/station.dat','w')
+done_list = []
 
 f=open(cfg.fsta); lines=f.readlines(); f.close()
 for line in lines:
     codes = line.split(',')
     net, sta = codes[0].split('.')
+    if sta in done_list: continue
     lat = float(codes[1])
     lon = float(codes[2])
     fout.write('{} {} {}\n'.format(sta, lat, lon))
+    done_list.append(sta)
 fout.close()
