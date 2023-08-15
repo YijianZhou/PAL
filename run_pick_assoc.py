60b9b411eb3b5ab3b41576c31c876492a222d745
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2023-08-14 20:58:37 -0700

    Update run_pick_assoc.py

--- run_pick_assoc.py
+++ run_pick_assoc.py
@@ -57,6 +57,7 @@ associator = associator_pal.TS_Assoc(\
     min_sta = cfg.min_sta,
     ot_dev = cfg.ot_dev,
     max_res = cfg.max_res,
+    max_drop = cfg.max_drop, 
     vp = cfg.vp)
 out_root = os.path.split(args.out_ctlg)[0]
 if not os.path.exists(out_root): os.makedirs(out_root)
